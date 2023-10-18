9bed4ec8a3954294dfd551c7ffa2ada2ff5d2e20
Smurk23 <xingcantres@gmail.com>
2023-10-17 22:13:09 -0400

    added final table

--- schema.sql
+++ schema.sql
@@ -17,3 +17,17 @@ CREATE TABLE role (
     REFERENCES department(id)
     ON DELETE CASCADE
 );
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    role_id INT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE CASCADE,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+);
