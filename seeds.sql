d7313a021aafaf7ad2a044e85aeb9bfb90e97476
Smurk23 <xingcantres@gmail.com>
2023-10-17 22:28:07 -0400

    finsihed off first two tables

---
+++ seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO department (name)
+VALUES ("engineering"),
+       ("finance"),
+       ("legal"),
+       ("sales");
+INSERT INTO role (department_id, title, salary)
+VALUES (4, "sales person", 100000),
+       (4, "sales lead", 150000),
+       (1, "lead engineer", 200000),
+       (2, "account manager", 75000),
+       (2, "accountant", 95000),
+       (3, "legal team lead", 250000),
+       (3, "lawyer", 100000);
+       
